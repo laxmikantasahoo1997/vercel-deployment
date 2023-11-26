24eb4aed533f4becc2cca29f047c0f0dfa3c83e0
laxmikanta1997 <laxmikanta@enhanzed.com>
2023-11-26 11:53:17 +0530

    vercel.json config changed

--- server/app.js
+++ server/app.js
@@ -10,7 +10,11 @@ dotenv.config(); // Load environment variables from .env file
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin:["https://deploy-mern-1whq.vercel.app"],
+  methods:["POST","GET"],
+  credentials:true
+}));
 app.use(express.json());
 
 // Connect to MongoDB
